fce094808dcf6ca12d4956e640ce8314eaf98c44
Truong <24728771.truong@student.iuh.edu.vn>
2026-03-26 23:54:29 +0700

    doi moi alert() sang showToast()

--- assets/src/js/core/auth.js
+++ assets/src/js/core/auth.js
@@ -32,7 +32,7 @@
             @keyframes slideIn {
                 from {
                     transform: translateY(100%);
-                    opacity: 0;
+                    opacity: 1;
                 }
                 to {
                     transform: translateY(0%);
